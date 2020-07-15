81ed57909730fe546985e097999c3fb44d18b40e
Timur Safin <tsafin@tarantool.org>
2020-07-15 15:19:03 +0300

    cmake changes
    - moved *.cmake to cmake/ firectory for better house keeping
    - commented out msgpuck - use bundled version instead
    - simplified Tarantool finder: we use source directory
      passed to us, and then all corresponding directories necessary for
      build

--- cmake/FindTarantool.cmake
+++ cmake/FindTarantool.cmake
@@ -8,14 +8,14 @@ macro(extract_definition name output input)
         ${output} "${_t}")
 endmacro()
 
-find_path(TARANTOOL_INCLUDE_DIR tarantool/module.h
-  HINTS ${TARANTOOL_DIR} ENV TARANTOOL_DIR
+find_path(TARANTOOL_INCLUDE_DIR src/module.h tarantool/module.h
+  HINTS ${TARANTOOL_BUILD_INCDIR} TARANTOOL_INCLUDE_DIRS ENV
   PATH_SUFFIXES include
 )
 
 if(TARANTOOL_INCLUDE_DIR)
     set(_config "-")
-    file(READ "${TARANTOOL_INCLUDE_DIR}/tarantool/module.h" _config0)
+    file(READ "${TARANTOOL_INCLUDE_DIR}/src/module.h" _config0)
     string(REPLACE "\\" "\\\\" _config ${_config0})
     unset(_config0)
     extract_definition(PACKAGE_VERSION TARANTOOL_VERSION ${_config})
@@ -23,16 +23,35 @@ if(TARANTOOL_INCLUDE_DIR)
     unset(_config)
 endif()
 
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TARANTOOL
     REQUIRED_VARS TARANTOOL_INCLUDE_DIR VERSION_VAR TARANTOOL_VERSION)
 if(TARANTOOL_FOUND)
-    set(TARANTOOL_INCLUDE_DIRS "${TARANTOOL_INCLUDE_DIR}"
-                               "${TARANTOOL_INCLUDE_DIR}/tarantool/"
+    set(TARANTOOL_INCLUDE_DIRS 
+            ${TARANTOOL_INCLUDE_DIR}
+            ${TARANTOOL_INCLUDE_DIR}/src/
+            ${TARANTOOL_INCLUDE_DIR}/../
+            ${TARANTOOL_INCLUDE_DIR}/../src/
+            ${TARANTOOL_INCLUDE_DIR}/../src/box/
+            ${TARANTOOL_INCLUDE_DIR}/../src/lib/core/
+            ${TARANTOOL_INCLUDE_DIR}/../src/lib/
+            ${TARANTOOL_INCLUDE_DIR}/../src/lib/small/
+            ${TARANTOOL_INCLUDE_DIR}/../src/lib/small/third_party/
+            ${TARANTOOL_INCLUDE_DIR}/../src/lib/msgpuck/
+            ${TARANTOOL_INCLUDE_DIR}/../third_party/
+            ${TARANTOOL_INCLUDE_DIR}/../third_party/luajit/src
+
         CACHE PATH "Include directories for Tarantool")
-    set(TARANTOOL_INSTALL_LIBDIR "${CMAKE_INSTALL_LIBDIR}/tarantool"
+
+    set (LUAJIT_BUNDLED_PREFIX  ${TARANTOOL_INCLUDE_DIR}/third_party/luajit/src)
+    set (LUAJIT_LIBRARIES       ${LUAJIT_BUNDLED_PREFIX}/libluajit.a)
+
+    set(TARANTOOL_INSTALL_LIBDIR 
+            ${CMAKE_INSTALL_LIBDIR}/tarantool
         CACHE PATH "Directory for storing Lua modules written in Lua")
-    set(TARANTOOL_INSTALL_LUADIR "${CMAKE_INSTALL_DATADIR}/tarantool"
+    set(TARANTOOL_INSTALL_LUADIR 
+            ${CMAKE_INSTALL_DATADIR}/tarantool
         CACHE PATH "Directory for storing Lua modules written in C")
 
     if (NOT TARANTOOL_FIND_QUIETLY AND NOT FIND_TARANTOOL_DETAILS)
@@ -41,5 +60,8 @@ if(TARANTOOL_FOUND)
         message(STATUS "Tarantool LIBDIR is ${TARANTOOL_INSTALL_LIBDIR}")
     endif ()
 endif()
-mark_as_advanced(TARANTOOL_INCLUDE_DIRS TARANTOOL_INSTALL_LIBDIR
-    TARANTOOL_INSTALL_LUADIR)
+mark_as_advanced(
+    TARANTOOL_INCLUDE_DIRS
+    TARANTOOL_INSTALL_LIBDIR
+    TARANTOOL_INSTALL_LUADIR
+)
